b4135eab38e452ba781451de6aa8a2720cc81f71
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-02-26 13:40:52 -0500

    Initial Commit

---
+++ Myth Buster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Myth Buster
+//
+//  Created by Fahim Uddin on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
