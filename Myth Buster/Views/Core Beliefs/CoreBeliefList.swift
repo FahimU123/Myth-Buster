053dee48f51fb539fba9e924038a2ee2f42250f2
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-02-26 14:27:42 -0500

    ...

--- Myth Buster/Views/Core Beliefs/CoreBeliefList.swift
+++ Myth Buster/Views/Core Beliefs/CoreBeliefList.swift
@@ -12,12 +12,12 @@ struct CoreBeliefsList: View {
         CoreBelief(
             title: "Belief in God (Allah)",
             shortDescription: "Belief in the one and only God, Allah.",
-            detailedDescription: "In Islam, the belief in God is the cornerstone of faith. This entails believing in one God, Allah, who is unique, without partners or equals. He is the Creator and Sustainer of the universe, all-knowing, all-powerful, and merciful..."
+            detailedDescription: "In Islam, the belief in God is the cornerstone of faith. This entails believing in one God, Allah, who is unique, without partners or equals. He is the Creator and Sustainer of the universe, all-knowing, all-powerful, and merciful."
         ),
         CoreBelief(
             title: "Belief in Angels",
             shortDescription: "Belief in the existence of angels.",
-            detailedDescription: "Angels are created beings of light who serve God. They are messengers, guardians, and executors of God's will. They do not have free will and are completely obedient to God..."
+            detailedDescription: "Angels are created beings of light who serve God. They are messengers, guardians, and executors of God's will. They do not have free will and are completely obedient to God."
         ),
   
     ]
